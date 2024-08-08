c2266e5ef654bb01f699763e3c922980c92597bd
igor pirasoli <igorpirasoli@outlook.com>
2024-08-07 22:57:35 -0300

    criando classe posiçao

---
+++ Jogo_de_xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using tabuleiro;
+namespace Jogo_de_xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
+        }
+    }
+}
